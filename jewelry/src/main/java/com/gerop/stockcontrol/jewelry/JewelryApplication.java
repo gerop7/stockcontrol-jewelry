30eabc208489f84057abdc262fc162bc4f3d1513
gerogg29 <geronimoggarcia2018@gmail.com>
2025-09-22 23:46:13 -0300

    first commit

---
+++ jewelry/src/main/java/com/gerop/stockcontrol/jewelry/JewelryApplication.java
@@ -0,0 +1,13 @@
+package com.gerop.stockcontrol.jewelry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelryApplication.class, args);
+	}
+
+}
