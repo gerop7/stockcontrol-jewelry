7168d8da19f5f540bb3db38ab81f893bd9e58df7
gerogg29 <geronimoggarcia2018@gmail.com>
2025-10-14 18:55:16 -0300

    adjusts

--- jewelry/src/main/java/com/gerop/stockcontrol/jewelry/repository/CategoryRepository.java
+++ jewelry/src/main/java/com/gerop/stockcontrol/jewelry/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.gerop.stockcontrol.jewelry.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.gerop.stockcontrol.jewelry.model.entity.Category;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long>{
+    Optional<Category> findByIdAndUserId(Long id, Long userId);
 }
--- jewelry/src/main/java/com/gerop/stockcontrol/jewelry/repository/SubcategoryRepository.java
+++ jewelry/src/main/java/com/gerop/stockcontrol/jewelry/repository/SubcategoryRepository.java
@@ -1,5 +1,7 @@
 package com.gerop.stockcontrol.jewelry.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.gerop.stockcontrol.jewelry.model.entity.Subcategory;
 
 @Repository
 public interface SubcategoryRepository extends CrudRepository<Subcategory, Long>{
+    Optional<Subcategory> findByIdAndUserId(Long id, Long userId);
 }
